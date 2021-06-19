c56ed20b14ac0e26f007dad9ec6df70fefd828c9
shery shawky <sheryshawky2018@gmail.com>
2021-06-19 02:38:56 +0200

    RadioButtons

---
+++ radio.js
@@ -0,0 +1,47 @@
+function ChangeFont(type) {
+
+    document.getElementById("PAR").style.fontFamily =type;
+
+  }
+  function ChangeAlign(type) {
+
+    document.getElementById("PAR").style.textAlign =type;
+
+  }
+  
+  function ChangeHeight(type) {
+
+    document.getElementById("PAR").style.lineHeight =type;
+
+  }
+  function ChangeLSpace(type) {
+
+    document.getElementById("PAR").style.letterSpacing =type;
+
+  }
+  
+  function ChangeIndent(type) {
+
+    document.getElementById("PAR").style.textIndent =type;
+
+  }
+  function ChangeTransform(type) {
+
+    document.getElementById("PAR").style.textTransform =type;
+
+  }
+  function ChangeDecorate(type) {
+
+    document.getElementById("PAR").style.textDecoration =type;
+
+  }
+  function ChangeBorder(type) {
+
+    document.getElementById("PAR").style.border=type;
+
+  }
+  function ChangeBorderColor(type) {
+
+    document.getElementById("PAR").style.borderColor =type;
+
+  }
